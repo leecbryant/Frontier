cfbf60511f7cc9c3b5d95117f680d9d3de522259
Lee Bryant <49053579+leecbryant@users.noreply.github.com>
2019-11-22 17:16:46 -0800

    Initial Commit

---
+++ Frontier/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Frontier
+//
+//  Created by Lee Bryant on 11/22/19.
+//  Copyright © 2019 Lee Bryant. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Frontier/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Frontier
+//
+//  Created by Lee Bryant on 11/22/19.
+//  Copyright © 2019 Lee Bryant. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
