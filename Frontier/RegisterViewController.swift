04f4358e64d9b0d639bfcf1b1337391e64f44e32
Lee Bryant <49053579+leecbryant@users.noreply.github.com>
2020-02-18 14:51:05 -0800

    Added cancel to registration page

--- Frontier/RegisterViewController.swift
+++ Frontier/RegisterViewController.swift
@@ -1,9 +1,22 @@
 //
-//  RegisterViewController.swift
+//  LoginViewController.swift
 //  Frontier
 //
-//  Created by Lee Bryant on 2/18/20.
-//  Copyright © 2020 Lee Bryant. All rights reserved.
+//  Created by Lee Bryant on 11/24/19.
+//  Copyright © 2019 Lee Bryant. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+}
