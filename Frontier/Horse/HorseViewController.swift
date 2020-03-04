9cb4efe2991b62543ebcee97980db079cd5c5bda
Lee Bryant <49053579+leecbryant@users.noreply.github.com>
2020-03-03 18:37:44 -0800

    Scrollview updates

--- Frontier/Horse/HorseViewController.swift
+++ Frontier/Horse/HorseViewController.swift
@@ -8,13 +8,12 @@
 
 import UIKit
 import class Kingfisher.KingfisherManager
-
+import class Kingfisher.ImageCache
 var Bands = ["Horse Band 1", "Horse Band 2", "Horse Band 3", "Horse Band 4", "Horse Band 5", "Horse Band 6","Horse Band 7", "Horse Band 8", "Horse Band 9", "Horse Band 10", "Horse Band 11", "Horse Band 12", "Horse Band 13", "Horse Band 14", "Horse Band 15", "Horse Band 16", "Horse Band 17", "Horse Band 18", "Horse Band 19", "Horse Band 20"]
 var filteredBands = [HorseData]()
 
 var images = [UIImage(named: "black"), UIImage(named: "bay")]
 class HorseViewController: UIViewController {
-    
     // Labels
     @IBOutlet weak var tableView: UITableView!
     // Image Definitions
@@ -26,8 +25,9 @@ class HorseViewController: UIViewController {
     var imageArray = [String]()
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
+        ImageScroller.reloadInputViews()
+
         filterBands()
         navigationItem.title = data[selectedIndex].Name
         
@@ -48,39 +48,69 @@ class HorseViewController: UIViewController {
             BandLabel.isHidden = true
         }
 
-        for i in 0..<imageArray.count {
-                   let imageView = UIImageView()
-                   // imageView.image = imageArray[i]
-                   imageView.kf.indicatorType = .activity
-                   imageView.kf.setImage(with: URL(string: imageArray[i])) { result in
-                           switch result {
-                               case .success(let value):
-                                    imageView.contentMode = .center;
-                                    if (self.ImageScroller.bounds.size.width < value.image.size.width && self.ImageScroller.bounds.size.height < value.image.size.height) {
-                                        imageView.contentMode = .scaleAspectFit;
-                                    }
-                               case .failure(let error):
-                                   print(error)
-                           }
-                    }
-            let xPos = self.view.bounds.width * CGFloat(i)
-                   imageView.frame = CGRect(x: xPos, y: 0, width: self.ImageScroller.frame.width, height: self.ImageScroller.frame.height)
-                    // imageView.contentMode = .scaleAspectFit
-                   
-                    self.ImageScroller.contentSize.width = self.ImageScroller.frame.width * CGFloat(i + 1)
-                    self.ImageScroller.addSubview(imageView)
-               }
-        
+        // Horse Images
+        ImageScroller.delegate = self
+        loadHorseImages()
         // Image Pager Setup
 //        ImagePager.numberOfPages = imageArray.count
 //        ImagePager.currentPage = 0
         
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+       super.viewWillAppear(animated)
+       loadHorseImages()
+    }
+
+    override func viewDidLayoutSubviews() {
+        self.ImageScroller.contentSize.width = self.ImageScroller.frame.width * CGFloat(imageArray.count + 1)
+    }
+    
+    func loadHorseImages() {
+        ImageScroller.subviews.forEach({ $0.removeFromSuperview() }) // this gets things done
+        for i in 0..<imageArray.count {
+            let imageView = UIImageView()
+            imageView.setNeedsDisplay()
+            ImageScroller.setNeedsDisplay()
+            let cache = ImageCache.default
+            // Checks if image is already cached
+             if(!cache.isCached(forKey: imageArray[i])) {
+                 imageView.kf.indicatorType = .activity
+                 imageView.kf.setImage(with: URL(string: imageArray[i])) { result in
+                     switch result {
+                     case .success( _):
+                            imageView.contentMode = .scaleAspectFit
+                            let xPosition = self.ImageScroller.frame.width * CGFloat(i)
+                            imageView.frame = CGRect(x: xPosition, y: 0, width: self.ImageScroller.frame.width, height: self.ImageScroller.frame.height)
+                            self.ImageScroller.contentSize.width = self.ImageScroller.frame.width * CGFloat(i)
+                            self.ImageScroller.addSubview(imageView)
+                         case .failure(let error):
+                             print(error) // The error happens
+                     }
+                 }
+             } else {
+                 cache.retrieveImage(forKey: imageArray[i]) { result in
+                     switch result {
+                         case .success(let value):
+                             imageView.image = value.image
+                             imageView.contentMode = .scaleAspectFit
+                             let xPosition = self.ImageScroller.frame.width * CGFloat(i)
+                             imageView.frame = CGRect(x: xPosition, y: 0, width: self.ImageScroller.frame.width, height: self.ImageScroller.frame.height)
+                             self.ImageScroller.contentSize.width = self.ImageScroller.frame.width * CGFloat(i + 1)
+                             self.ImageScroller.addSubview(imageView)
+                         case .failure(let error):
+                             print(error)
+                     }
+                 }
+             }
+        }
     }
     
     func filterBands() {
         filteredBands = data.filter({ horse -> Bool in
             return horse.Band == data[selectedIndex].Band && horse.Name != data[selectedIndex].Name
         })
+        filteredBands.sort(by: {$0.Name < $1.Name})
     }
 }
 
@@ -102,11 +132,14 @@ extension HorseViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("Row Selected with id: \(filteredBands[indexPath.row].id)")
+//        print("Row Selected with id: \(filteredBands[indexPath.row].id)")
         selectedIndex = filteredBands[indexPath.row].id
-        self.viewDidLoad()
-        self.viewWillAppear(true)
-        filterBands()
-        tableView.reloadData()
+//        self.viewDidLoad()
+//        self.viewWillAppear(true)
+//        tableView.reloadData()
+
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "HorseViewController") as! HorseViewController
+            vc.data = data
+        self.navigationController?.pushViewController(vc, animated: true)
     }
 }
